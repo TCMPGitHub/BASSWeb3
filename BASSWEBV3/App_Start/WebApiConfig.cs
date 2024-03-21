28a00ac9d5cb563e273f3d00e638c29d3395266b
zhx045@health.ucsd.edu <zhx045@health.ucsd.edu>
2024-03-21 16:29:42 -0700

    Facility and BW productivity reports modified.

--- BASSWEBV3/App_Start/WebApiConfig.cs
+++ BASSWEBV3/App_Start/WebApiConfig.cs
@@ -5,7 +5,7 @@ namespace BASSWEBV3.App_Start
 {
     public static class WebApiConfig
     {
-        public static void Register(HttpConfiguration config)
+		public static void Register(HttpConfiguration config)
         {
             // Web API routes
             //config.MapHttpAttributeRoutes();
--- BASSWEBV3/Controllers/ValuesController.cs
+++ BASSWEBV3/Controllers/ValuesController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace BASSWEBV3.Controllers
